bcd2354ede8a694bf855e811c0c1e7eb3d49a66a
itgeeksromm <itgeek@email.com>
2023-06-10 23:03:52 -0400

    new

--- src/main.tf
+++ src/main.tf
@@ -2,7 +2,7 @@ terraform {
   # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
   # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
   backend "s3" {
-    bucket         = "cc-tf-state-backend-cicd-new"
+    bucket         = "guru-2-bucket"
     key            = "tf-infra/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-locking"
@@ -18,12 +18,21 @@ terraform {
   }
 }
 
-
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 module "tf-state" {
-    source = "./modules/tf-state"
-    bucket_name = "cc-tf-state-backend-cicd-new"
+  source      = "./modules/tf-state"
+  bucket_name = "guru-2-bucket"
 }
+
+# module "vpc-infra" {
+#   source = "./modules/vpc"
+
+#   # VPC Input Vars
+#   vpc_cidr             = local.vpc_cidr
+#   availability_zones   = local.availability_zones
+#   public_subnet_cidrs  = local.public_subnet_cidrs
+#   private_subnet_cidrs = local.private_subnet_cidrs
+# }
