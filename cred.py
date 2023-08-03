be5c51b60ac43af7ba7530730f44952a88a15a0b
smakineni-panw <111462735+smakineni-panw@users.noreply.github.com>
2023-08-03 13:06:32 -0400

    Update cred.py

--- cred.py
+++ cred.py
@@ -1,3 +1,7 @@
 [default]
 aws_access_key_id = AKIATEAK4HLV2R55WTHC
 aws_secret_access_key = 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
+
+[new]
+aws_access_key_id = AKIATEAK4HLV2R55WTHC
+aws_secret_access_key = 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
